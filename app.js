dfb22f39c0d54cfc2a660b0800ffed90414d84f9
Jacob_Wendt <jacob.wendt@yahoo.com>
2019-06-12 15:12:13 -0500

    Added error.js and event.js and replaced throws

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 'use strict';
 
+
+const event = require('./events/event');
+require('./events/error');
 const fs = require('fs');
 
 const alterFile = (file) => {
   fs.readFile( file, (err, data) => {
-    if(err) { throw err; }
+    if(err) { event.emit('error', err) }
     let text = data.toString().toUpperCase();
     fs.writeFile( file, Buffer.from(text), (err, data) => {
-      if(err) { throw err; }
+      if(err) { event.emit('error', err) }
       console.log(`${file} saved`);
     });
   });
